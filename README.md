f380fb762e104485db396b7ed9ccd6f73a47e5ac
Aaron D Borden <adborden@a14n.net>
2018-11-26 15:51:36 -0800

    Logging, argument parsing, and request sessions

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # datagov-dedupe
+
+Delete duplicate packages on data.gov.
+
+
+## Usage
+
+Install the dependencies.
+
+    $ pip install -r requirements.txt
+
+Deduplicate packages for a specific organization.
+
+    $ python duplicates-identifier-api.py [organization-name]
+
+Scan all organizations and dedupe packges for each.
+
+    $ python duplicates-identifier-api.py
+
+
+## Development
+
+Install the latest dependencies.
+
+    $ pip install -r requirements.in
--- duplicates-identifier-api.py
+++ duplicates-identifier-api.py
@@ -1,33 +1,62 @@
-import json
-import requests
-import sys
-import datetime
-import pdb
-import csv
+
+import argparse
+import logging
 import os
-import urllib2
 import time
-import psycopg2
+
+import requests
+
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger('dedupe')
 
 
-def get_org_list(url):
+class CkanApiException(Exception):
+    def __init__(self, message, response):
+        super(CkanApiException, self).__init__(message)
+        self.response = response
+
+
+class CkanApiClient(object):
+    '''
+    Represents a client to query and submit requests to the CKAN API.
+    '''
+
+    def __init__(self, api_url, api_key):
+        self.api_url = api_url
+        self.client = requests.Session()
+        self.client.headers.update(Authorization=api_key)
+
+    def request(self, method, path, **kwargs):
+        log.debug('Api request method=%s path=%s', method, path)
+        url = '%s/api%s' % (self.api_url, path)
+        response = self.client.request(method, url, **kwargs)
+        if not response.status_code == 200 or not response.json()['success']:
+            log.error('CKAN API request failed response=%s', response.content)
+            raise CkanApiException('CKAN API request failed.', response)
+
+
+        return response
+
+    def get(self, path, **kwargs):
+        return self.request('GET', path, **kwargs)
+
+
+def get_org_list(ckan):
     organizations_list = []
 
-    org_list = requests.get(url + "/api/action/package_search?q=source_type:datajson&rows=1000")
+    log.debug('Fetching organizations...')
+    org_list = ckan.get("/action/package_search?q=source_type:datajson&rows=1000")
     org_list = org_list.json()['result']['results']
 
     for organization in org_list:
         if organization['organization']['name'] not in organizations_list:
             organizations_list.append(organization['organization']['name'])
-    organizations_list.sort()
 
-    #with open('org_out.txt', 'w') as f:
-    #    print >> f, 'Filename:', organizations_list
-
+    log.debug('Found organizations count=%d', len(organizations_list))
     return organizations_list
 
 
-def get_dataset_list(datagov_url, org_name):
+def get_dataset_list(ckan, org_name):
     '''
         Get the datasets on data.gov that we have for the organization
     '''
@@ -40,28 +69,48 @@ def get_dataset_list(datagov_url, org_name):
     dup_log = []
     dup_json_log = []
 
+
     # get list of harvesters for the organization
-    org_harvest_tmp = requests.get(
-        datagov_url + "/api/3/action/package_search?q=organization:" + org_name +
-        "&facet.field=[%22identifier%22]&facet.limit=-1&facet.mincount=2")
+    log.info('Fetching harvesters for organization=%s', org_name)
+    org_harvest_tmp = ckan.get('/3/action/package_search', params={
+        'q': 'organization:%s' % org_name,
+        'facet.field': '["identifier"]',
+        'facet.limit': -1,
+        'facet.mincount': 2,
+        })
+
+    log.debug('%r', org_harvest_tmp)
     org_harvest_tmp = org_harvest_tmp.json()['result']['search_facets']['identifier']['items']
+    log.info('Found harvest identifiers for organization=%s count=%d', org_name, len(org_harvest_tmp))
 
     for harvest in org_harvest_tmp:
         org_harvest.append(harvest['name'])
 
     for identifier in org_harvest:
-        dataset_list = requests.get(
-            datagov_url + '/api/action/package_search?q=identifier:"' + identifier + '"' +
-            '&fq=type:dataset&sort=metadata_created+desc&rows=1000')
+        log.info('Fetching count of datasets for harvest organization=%s identifier=%s',
+                 org_name, identifier)
+        dataset_list = ckan.get('/action/package_search', params={
+            'q': 'identifier:"%s"' % identifier,
+            'fq': 'type:dataset',
+            'sort': 'metadata_created+desc',
+            'rows': 0,
+            })
+
         harvest_data_count = dataset_list.json()['result']['count']
         start = 0
+        rows = 1000
         while start <= harvest_data_count:
             try:
-                dataset_list = requests.get(
-                    datagov_url + '/api/action/package_search?q=identifier:"' + identifier + '"&start=' + str(
-                        start) + '&rows=1000')
+                log.info(
+                    'Batch fetching datasets for harvest organization=%s identifier=%s offset=%d rows=%d',
+                    org_name, identifier, start, rows)
+                dataset_list = ckan.get('/action/package_search', params={
+                    'q': 'identifier:"%s"' % identifier,
+                    'start': start,
+                    'rows': rows,
+                    })
                 dataset_harvest_list += dataset_list.json()['result']['results']
-                start += 1000
+                start += rows
             except IndexError:
                 time.sleep(20)
                 continue
@@ -84,44 +133,10 @@ def get_dataset_list(datagov_url, org_name):
             if dataset_harvest['id'] not in totla_dup_data and dataset_harvest['organization']['name'] == org_name:
                 totla_dup_data.append(dataset_harvest['id'])
 
-        duplicates = list(set(totla_dup_data) - set(dataset_keep))
+        duplicates += list(set(totla_dup_data) - set(dataset_keep))
 
 
-    for dataset in duplicates:
-        for dataset_h in dataset_harvest_list:
-            if dataset_h['id'] == dataset:
-                for extra in dataset_h['extras']:
-                    # get the harvest_id
-                    if extra['key'] == 'identifier':
-                        identifier = extra['value']
-                    if extra['key'] == 'source_hash':
-                        source_hash = str(extra['value'])
-                        dup_json_log.append(dataset_h)
-                        dup_log.append("" + dataset_h['id'] + "', '" + dataset_h['name'] + "', '" + dataset_h['title']
-                                       + "', '" + ("https://catalog.data.gov/dataset/" + dataset_h['name']) 
-                                       + "', '" + dataset_h['metadata_created'] + "', '" + identifier + "', '" +
-                                       source_hash + "', 'duplicate-removed")
-    with open('duplicates_datasets_json_' + org_name + '.txt', 'w') as f:
-        print >> f, 'Filename:', dup_json_log
-
-    with open('duplicates_datasets_' + org_name + '.csv', "w") as csv_f:
-        fieldnames = ['Id', 'Name', 'Title', 'URL', 'Metadata Created', 'Identifier', 'Source Hash', 'Action']
-
-        writer = csv.writer(csv_f, lineterminator='\n')
-        writer.writerow(fieldnames)
-        for dslist in dup_log:
-            temp_list = dslist.split("', '")
-            temp_list = (temp_list[0].encode('ascii', 'ignore').decode('ascii') + "', '" +
-                         temp_list[1].encode('ascii', 'ignore').decode('ascii') + "', '" +
-                         temp_list[2].encode('ascii', 'ignore').decode('ascii') + "', '" +
-                         temp_list[3].encode('ascii', 'ignore').decode('ascii') + "', '" +
-                         temp_list[4].encode('ascii', 'ignore').decode('ascii') + "', '" +
-                         temp_list[5].encode('ascii', 'ignore').decode('ascii') + "', '" +
-                         temp_list[6].encode('ascii', 'ignore').decode('ascii') + "', '" +
-                         temp_list[7].encode('ascii', 'ignore').decode('ascii'))
-            writer.writerow(temp_list.split("', '"))
-#            writer.writerow([dslist.encode('ascii', 'ignore').decode('ascii')])
-
+    log.info('Found duplicate datasets organization=%s count=%d', org_name, len(duplicates))
     return duplicates
 
 #def remove_duplicate_datasets(duplicate_datasets, o_name,sysadmin_api_key):
@@ -140,26 +155,32 @@ def remove_duplicate_datasets(duplicate_datasets):
 
     # conn.close()
 
-
-if __name__ == "__main__":
+def run():
     '''
         This code for getting the list of organizations and duplicate duplicate data sets
     '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--api-key', default=os.getenv('CKAN_API_KEY', None), help='Admin API key')
+    parser.add_argument('--api-url', default='https://admin-catalog.data.gov',
+                        help='The API base URL to query')
+    parser.add_argument('organization_id', default=None,
+                        help='The API base URL to query')
 
-    datagov_url = 'http://catalog.data.gov'
-    #datagov_url = 'http://catalog-web-test.datagov.us'
-    sysadmin_api_key = '<my_admin_api_key>'
-    duplicate_datasets = []
-    datagov_datasets = []
-    # get organizations that have datajson harvester
-    org_list = get_org_list(datagov_url)
+    args = parser.parse_args()
+
+    ckan = CkanApiClient(args.api_url, args.api_key)
+
+    if args.organization_id:
+        org_list = [args.organization_id]
+    else:
+        # get all organizations that have datajson harvester
+        org_list = get_org_list(ckan)
 
     # get list of duplicate_datasets
     for organization in org_list:
-        dataset_dup_tmp = get_dataset_list(datagov_url, organization)
-        duplicate_datasets += dataset_dup_tmp
-        # remove_duplicate_datasets(duplicate_datasets,organization, sysadmin_api_key,)
-        remove_duplicate_datasets(dataset_dup_tmp)
+        dataset_dup_tmp = get_dataset_list(ckan, organization)
+        #remove_duplicate_datasets(dataset_dup_tmp)
 
-    # remove_duplicate_datasets(duplicate_datasets, sysadmin_api_key)
-    #remove_duplicate_datasets(duplicate_datasets)
+
+if __name__ == "__main__":
+    run()
